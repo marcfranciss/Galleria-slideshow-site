28e73ea2e1bf056a9517926c1093aa1ef89d8364
marcfranciss <marcfrancissurban@gmail.com>
2024-10-13 23:50:17 +0800

    update: fix images for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Galleria-slideshow-site/",
+  base: "https://marcfranciss.github.io/Galleria-slideshow-site/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
